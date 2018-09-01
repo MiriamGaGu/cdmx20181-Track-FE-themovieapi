cc81a6a6a6a222236257e1922e4dc083eb9d1c47
Miriam <mgg_pjl@yahoo.com>
2018-08-31 19:23:17 -0500

    Agregando firebase, cambios en json

---
+++ JS/firebase.config.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyASZXgCEGdAMAid46mN08NpNQQfUT_v_bw",
+    authDomain: "horrormovies-68cd4.firebaseapp.com",
+    databaseURL: "https://horrormovies-68cd4.firebaseio.com",
+    projectId: "horrormovies-68cd4",
+    storageBucket: "horrormovies-68cd4.appspot.com",
+    messagingSenderId: "789670674156"
+  };
+  firebase.initializeApp(config);
